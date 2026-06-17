6c39fcf7f38c59abc6b53931af7f910653e4d9bd
siddardhach2005 <siddharthachennemsetti@gmail.com>
2026-06-17 14:38:52 +0530

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int sos(int n){
+        int s=0;
+        while(n>0){
+            int d=n%10;
+            s+=d*d;
+            n/=10;
+        }
+        return s;
+    }
+    bool isHappy(int n) {
+        unordered_set<int> vis;
+        while(vis.find(n)==vis.end() && n!=1){
+            vis.insert(n);
+            n=sos(n);
+        }
+        return n==1;
+    }
+};
