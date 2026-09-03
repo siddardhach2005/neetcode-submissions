6742f0eea1f8248c81180ebb4a0cd1517a84ace7
siddardhach2005 <siddharthachennemsetti@gmail.com>
2026-09-03 21:28:08 +0530

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    pair<int, int> pal(string& s, int l, int r) {
+        while (l >= 0 && r < s.size() && s[l] == s[r]) {
+            l--;
+            r++;
+        }
+        return {l + 1, r - l - 1};
+    }
+    string longestPalindrome(string s) {
+        int len = 1;
+        int st = 0;
+        for (int i = 0; i < s.size(); i++) {
+            auto odd = pal(s, i, i);
+            auto even = pal(s, i, i + 1);
+            if (odd.second > len) {
+                st = odd.first;
+                len = odd.second;
+            }
+            if (even.second > len) {
+                st = even.first;
+                len = even.second;
+            }
+        }
+        return s.substr(st, len);
+    }
+};
